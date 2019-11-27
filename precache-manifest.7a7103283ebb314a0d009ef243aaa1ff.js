bfc8fad22af425b720e8473b3ca958f2c91958b4
Alberto Mata Espiga <alberto.mata@idiso.com>
2019-11-27 17:21:40 +0100

    Updates

---
+++ precache-manifest.7a7103283ebb314a0d009ef243aaa1ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e97df1e6d64455bc6e55d027d9f3e983",
+    "url": "/codename-alfarius/index.html"
+  },
+  {
+    "revision": "5c232bc26976041361ab",
+    "url": "/codename-alfarius/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "2146ae6520b367585dae",
+    "url": "/codename-alfarius/static/js/2.c0235b02.chunk.js"
+  },
+  {
+    "revision": "5c232bc26976041361ab",
+    "url": "/codename-alfarius/static/js/main.358077a9.chunk.js"
+  },
+  {
+    "revision": "ea57a554e6d3f7bdc078",
+    "url": "/codename-alfarius/static/js/runtime-main.7f544927.js"
+  }
+]);
