52a357f04e84bfd6d1471e54004f16b64f9c503b
Alberto Mata Espiga <alberto.mata@idiso.com>
2019-11-28 12:58:46 +0100

    finished the google oauth

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,20 +1,36 @@
 import React from "react";
 
-const onSignIn = googleUser => {
-  // Useful data for your client-side scripts:
-  var profile = googleUser.getBasicProfile();
-  console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-  console.log("Full Name: " + profile.getName());
-  console.log("Given Name: " + profile.getGivenName());
-  console.log("Family Name: " + profile.getFamilyName());
-  console.log("Image URL: " + profile.getImageUrl());
-  console.log("Email: " + profile.getEmail());
+const oauthSignIn = () => {
+  // Google's OAuth 2.0 endpoint for requesting an access token
+  let oauth2Endpoint = "https://accounts.google.com/o/oauth2/v2/auth";
 
-  // The ID token you need to pass to your backend:
-  var id_token = googleUser.getAuthResponse().id_token;
-  console.log("ID Token: " + id_token);
+  // Create <form> element to submit parameters to OAuth 2.0 endpoint.
+  let form = document.createElement("form");
+  form.setAttribute("method", "GET"); // Send as a GET request.
+  form.setAttribute("action", oauth2Endpoint);
+
+  // Parameters to pass to OAuth 2.0 endpoint.
+  let params = {
+    client_id:
+      "455339909818-q4d9d2j0hkncdc9mnog1tl5of6rupg5o.apps.googleusercontent.com",
+    redirect_uri: "http://localhost:3000/LoggedIn",
+    response_type: "token",
+    scope: "profile email openid",
+    include_granted_scopes: "true"
+  };
+
+  // Add form parameters as hidden input values.
+  for (let p in params) {
+    let input = document.createElement("input");
+    input.setAttribute("type", "hidden");
+    input.setAttribute("name", p);
+    input.setAttribute("value", params[p]);
+    form.appendChild(input);
+  }
+
+  // Add form to page and submit it to open the OAuth 2.0 endpoint.
+  document.body.appendChild(form);
+  form.submit();
 };
 
-export default () => (
-  <div class="g-signin2" data-onsuccess={onSignIn} data-theme="dark"></div>
-);
+export default () => <button onClick={oauthSignIn}>Loggeanízate!</button>;
